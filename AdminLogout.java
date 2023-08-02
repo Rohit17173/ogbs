b35b119832f70c9dfb08df460ebee067463bec23
Rohit17173 <95220899+Rohit17173@users.noreply.github.com>
2023-08-02 17:18:03 +0530

    Create AdminLogout.java

---
+++ AdminLogout.java
@@ -0,0 +1,27 @@
+package admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/adminLogout")
+public class AdminLogout extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            // Invalidate the session to log out the admin
+            session.invalidate();
+        }
+        // Redirect to the login page after logout
+        response.sendRedirect("adminlogin.jsp");
+    }
+}
+
